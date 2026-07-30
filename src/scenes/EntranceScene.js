4a50a78628bc48dfe4ef4302988301689807e217
katochin <kato@gluegent.com>
2026-07-30 18:01:40 +0900

    Balance MADMAX lettering proportions

--- src/scenes/EntranceScene.js
+++ src/scenes/EntranceScene.js
@@ -7,7 +7,7 @@ import { VibraSensorController } from "../input/VibraSensorController.js?v=0011c
 import { MADMAXButtonController } from "../input/MADMAXButtonController.js?v=0012c";
 import { DragSpring } from "../input/DragSpring.js";
 import { NorenRenderer } from "../renderers/NorenRenderer.js?v=0022c";
-import { PoolRenderer } from "../renderers/PoolRenderer.js?v=0012d";
+import { PoolRenderer } from "../renderers/PoolRenderer.js?v=0012e";
 import { initializeAudio } from "./EntranceAudioBootstrap.js?v=0012c";
 
 export class EntranceScene extends Scene {
